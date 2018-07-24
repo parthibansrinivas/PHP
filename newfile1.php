0af10a8e15e685a536e009ca671d4c646e9565a2
parthibansrinivas <40590314+parthibansrinivas@users.noreply.github.com>
2018-07-24 11:42:06 +0530

    Add files via upload

---
+++ newfile1.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $txt1 = "Welcome To Php";
+$txt2 = "PARTHIBAN";
+$x = 55;
+$y = 44;
+print "<h2>$txt1</h2>";
+print "Study PHP at $txt2<br>";
+print "The added output is";
+print $x + $y ;
+        ?>
+    </body>
+</html>
---
+++ newfile2.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        $x = "Hello world!";
+$y = 'how are you';
+echo $x;
+echo "<br>"; 
+echo $y;echo "<br>"; 
+$x = 6383;
+var_dump($x);
+        ?>
+    </body>
+</html>
+
